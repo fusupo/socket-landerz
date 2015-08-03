4f3271ecc1ee8c2be24688ea503ba6fdd5e2f333
Marc Christophe <m@xtof.net>
2015-08-03 13:37:26 -0700

    updating port specification; express hello world

--- server/server.js
+++ server/server.js
@@ -1,11 +1,14 @@
 var express = require('express');
 var app = express();
 
+var port = process.env.PORT || 3000;
+
 app.get('/', function(req, res) {
   res.send('Hello World!');
 });
 
-var server = app.listen(3000, function() {
+
+var server = app.listen(port, function() {
   var host = server.address().address;
   var port = server.address().port;
 
